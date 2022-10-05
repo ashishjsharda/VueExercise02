9bb5fb5484cd0fb39e791fc0260f230b2ed4818c
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-05 06:10:31 -0500

    Add title

--- src/components/Exercise1-02.vue
+++ src/components/Exercise1-02.vue
@@ -1,5 +1,7 @@
 <template>
-
+<div>
+  <h1> {{title}}</h1>
+</div>
 </template>
 
 <script>
@@ -7,7 +9,12 @@ export default {
   name: 'Exercise1-02',
   data() {
     return {
+      title: 'Exercise 1-02',
       message: 'Hello Vue.js!'
     }
   }
 }
+
+<style>
+
+</style>
